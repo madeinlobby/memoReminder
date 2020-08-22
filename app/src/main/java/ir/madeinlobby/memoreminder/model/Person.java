e275fd097666f016dbba7446fcfda35b4efb7d0e
aboots <mahdi.abootorabi2@gmail.com>
2020-08-22 19:45:57 +0430

    work on person and post class

--- app/src/main/java/ir/madeinlobby/memoreminder/model/Person.java
+++ app/src/main/java/ir/madeinlobby/memoreminder/model/Person.java
@@ -1,4 +1,21 @@
 package ir.madeinlobby.memoreminder.model;
 
+import java.util.ArrayList;
+
 public class Person {
+    private String username;
+    private String password;
+    private ArrayList<String> allPostsIds;
+    private ArrayList<Tag> allTags;
+    private String firstName;
+    private String lastName;
+
+    public Person(String username, String password, String firstName, String lastName) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.allPostsIds = new ArrayList<>();
+        this.allTags = new ArrayList<>();
+    }
 }
--- app/src/main/java/ir/madeinlobby/memoreminder/model/Post.java
+++ app/src/main/java/ir/madeinlobby/memoreminder/model/Post.java
@@ -1,4 +1,35 @@
 package ir.madeinlobby.memoreminder.model;
 
+import android.location.Location;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Post {
+    private String id;
+    private String title;
+    private String usernameWhoBelong;
+    private LocalDate dateCreated;
+    private LocalDate lastDateEdited;
+    private Location location;
+    private ArrayList<Tag> tags;
+    private ArrayList<String> usersWhoBeenTagged;
+    private ArrayList<String> commentsIds;
+    // image and video and voice
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public Post(String title, String usernameWhoBelong,Location location) {
+        this.title = title;
+        this.dateCreated = LocalDate.now();
+        this.lastDateEdited = LocalDate.now();
+        this.usernameWhoBelong = usernameWhoBelong;
+        this.id = RandomString.getAlphaNumericString();
+        this.tags = new ArrayList<>();
+        this.usersWhoBeenTagged = new ArrayList<>();
+        this.commentsIds = new ArrayList<>();
+        this.location = location;
+    }
 }

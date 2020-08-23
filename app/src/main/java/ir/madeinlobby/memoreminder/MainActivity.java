d243d84d369348cf360842dcb33ea59d36a6dd8c
ASUS <yasamanzolfi4@gmail.com>
2020-08-23 14:25:57 +0430

    login page

--- app/src/main/java/ir/madeinlobby/memoreminder/MainActivity.java
+++ app/src/main/java/ir/madeinlobby/memoreminder/MainActivity.java
@@ -3,6 +3,10 @@ package ir.madeinlobby.memoreminder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.register);
+        SpannableString content = new SpannableString( "create an account" ) ;
+        content.setSpan( new UnderlineSpan() , 0 , content.length() , 0 ) ;
+        textView.setText(content) ;
+    }
+
+    public void registerPressed(View view) {
+    }
+
+    public void loginClicked(View view) {
     }
 }

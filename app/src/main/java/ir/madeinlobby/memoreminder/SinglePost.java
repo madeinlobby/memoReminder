f5491e85addfcf6b64480bf467b3bc2a3865dd3b
ASUS <yasamanzolfi4@gmail.com>
2020-08-24 23:03:27 +0430

    start single post

---
+++ app/src/main/java/ir/madeinlobby/memoreminder/SinglePost.java
@@ -0,0 +1,18 @@
+package ir.madeinlobby.memoreminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class SinglePost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_post);
+    }
+
+    public void tagIconClicked(View view) {
+    }
+}

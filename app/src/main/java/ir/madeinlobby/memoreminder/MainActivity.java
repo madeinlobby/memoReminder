a55bcce3a71bb575bc25e9713855f1d2f18c788e
aboots <mahdi.abootorabi2@gmail.com>
2020-08-24 16:29:44 +0430

    send requests to server for register and login

--- app/src/main/java/ir/madeinlobby/memoreminder/MainActivity.java
+++ app/src/main/java/ir/madeinlobby/memoreminder/MainActivity.java
@@ -7,10 +7,17 @@ import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.util.HashMap;
+
+import ir.madeinlobby.memoreminder.utilities.HttpUtility;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String server="localhost:80";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +35,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void loginClicked(View view) {
+        EditText textUserName = findViewById(R.id.usernameForLogin);
+        EditText textPassword = findViewById(R.id.passwordForLogin);
+        String userName = textUserName.getText().toString();
+        String password = textPassword.getText().toString();
+        try {
+            HttpUtility.openConnection(server+"/login.php");
+            HashMap<String,String> fields=new HashMap<>();
+            fields.put("username",userName);
+            fields.put("password",password);
+            HttpUtility.sendPostRequest(fields);
+            String response = HttpUtility.readResponse();
+            if (response.startsWith("error")){
+                Toast.makeText(MainActivity.this,"invalid username or password",Toast.LENGTH_LONG).show();
+            }else{
+                String token = response.substring(4);
+                Toast.makeText(MainActivity.this,"login successful",Toast.LENGTH_LONG).show();
+            }
+        } catch (IOException e) {
+            Toast.makeText(MainActivity.this,"error in opening connection",Toast.LENGTH_LONG).show();
+        }
     }
 }
--- app/src/main/java/ir/madeinlobby/memoreminder/RegisterActivity.java
+++ app/src/main/java/ir/madeinlobby/memoreminder/RegisterActivity.java
@@ -2,11 +2,19 @@ package ir.madeinlobby.memoreminder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import ir.madeinlobby.memoreminder.utilities.HttpUtility;
 
 public class RegisterActivity extends AppCompatActivity {
+    private static final String server = "localhost:80";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +29,34 @@ public class RegisterActivity extends AppCompatActivity {
         EditText textLastName = findViewById(R.id.lastNameForRegister);
         EditText textEmail = findViewById(R.id.emailForRegister);
         String userName = textUserName.getText().toString();
-        String passWord = textPassword.getText().toString();
+        String password = textPassword.getText().toString();
         String firstName = textFirstName.getText().toString();
         String lastName = textLastName.getText().toString();
         String email = textEmail.getText().toString();
+        try {
+            HttpUtility.openConnection(server + "/login.php");
+            HashMap<String, String> fields = new HashMap<>();
+            fields.put("username", userName);
+            fields.put("password", password);
+            fields.put("firstName", firstName);
+            fields.put("lastName", lastName);
+            fields.put("email", email);
+            HttpUtility.sendPostRequest(fields);
+            String response = HttpUtility.readResponse();
+            if (response.startsWith("error")) {
+                if (response.equals("error: register_error, username_already_taken")) {
+                    Toast.makeText(RegisterActivity.this, "username taken already", Toast.LENGTH_LONG).show();
+                } else if (response.equals("error: register_error, empty_required_field")) {
+                    Toast.makeText(RegisterActivity.this, "every fields should be filled", Toast.LENGTH_LONG).show();
+                }
+                Toast.makeText(RegisterActivity.this, "invalid username or password", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(RegisterActivity.this, "register successful successful", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        } catch (IOException e) {
+            Toast.makeText(RegisterActivity.this, "error in opening connection", Toast.LENGTH_LONG).show();
+        }
     }
 }

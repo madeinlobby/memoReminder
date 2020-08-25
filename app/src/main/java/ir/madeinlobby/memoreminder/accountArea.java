c196f3fa8cdf9208a728a9cae440a9b8dc50c321
ASUS <yasamanzolfi4@gmail.com>
2020-08-26 00:34:32 +0430

    post row icons

---
+++ app/src/main/java/ir/madeinlobby/memoreminder/accountArea.java
@@ -0,0 +1,14 @@
+package ir.madeinlobby.memoreminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class accountArea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_area);
+    }
+}

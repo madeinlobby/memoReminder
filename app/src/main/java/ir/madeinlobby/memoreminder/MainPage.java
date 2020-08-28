e0fdb4446e71db118824d1dde51b390edaf7a28b
aboots <mahdi.abootorabi2@gmail.com>
2020-08-28 15:55:17 +0430

    add pickerColor

--- app/src/main/java/ir/madeinlobby/memoreminder/MainPage.java
+++ app/src/main/java/ir/madeinlobby/memoreminder/MainPage.java
@@ -3,20 +3,27 @@ package ir.madeinlobby.memoreminder;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import petrov.kristiyan.colorpicker.ColorPicker;
+
 public class MainPage extends AppCompatActivity {
-    LinearLayout linearLayout;
+    ScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
-        linearLayout = findViewById(R.id.main_layout);
+        scrollView = findViewById(R.id.main_layout);
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
             @Override
@@ -52,6 +59,28 @@ public class MainPage extends AppCompatActivity {
     }
 
     private void showTagsPage() {
+        LayoutInflater inflater;
+        inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.tags_page , null);
+        scrollView.addView(layout);
+    }
+
+    public void colorPickerClicked(View view){
+        ColorPicker colorPicker = new ColorPicker(MainPage.this);
+        colorPicker.setRoundColorButton(true);
+        colorPicker.setColorButtonMargin(10,10,10,10);
+        colorPicker.show();
+        colorPicker.setOnChooseColorListener(new ColorPicker.OnChooseColorListener() {
+            @Override
+            public void onChooseColor(int position,int color) {
+                
+            }
+
+            @Override
+            public void onCancel(){
+                // put code
+            }
+        });
     }
 
     private void contacts() {

b32bb94bdc823cdbff49fcb6fc85c0a299c99f85
aboots <mahdi.abootorabi2@gmail.com>
2020-08-29 15:24:51 +0430

    try to fix bugs

--- app/src/main/java/ir/madeinlobby/memoreminder/MainPage.java
+++ app/src/main/java/ir/madeinlobby/memoreminder/MainPage.java
@@ -2,12 +2,11 @@ package ir.madeinlobby.memoreminder;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,6 +14,7 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.Toast;
+
 
 import com.azeesoft.lib.colorpicker.ColorPickerDialog;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -45,18 +45,17 @@ public class MainPage extends AppCompatActivity {
                         selectedFragment = new HomePageFragment();
                         break;
                     case R.id.nav_contacts:
-                        scrollView.removeAllViews();
                         contacts();
                         break;
                     case R.id.nav_tags:
                         selectedFragment = new TagsPageFragment();
                         break;
                     case R.id.nav_tagged:
-                        scrollView.removeAllViews();
                         showTaggedPage();
                         break;
 
                     case R.id.nav_logout:
+//                        scrollView.removeAllViews();
                         logout();
                         break;
                 }
--- app/src/main/java/ir/madeinlobby/memoreminder/TagsPageFragment.java
+++ app/src/main/java/ir/madeinlobby/memoreminder/TagsPageFragment.java
@@ -4,10 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 
 public class TagsPageFragment extends Fragment {
     @Nullable

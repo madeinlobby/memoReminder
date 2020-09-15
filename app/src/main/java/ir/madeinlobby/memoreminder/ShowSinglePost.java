a88dc372dbb6b62e478ad02cf9f000191b3aa344
aboots <mahdi.abootorabi2@gmail.com>
2020-09-15 14:18:21 +0430

    work on show comments and add comments

--- app/src/main/java/ir/madeinlobby/memoreminder/ShowSinglePost.java
+++ app/src/main/java/ir/madeinlobby/memoreminder/ShowSinglePost.java
@@ -14,19 +14,28 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.beloo.widget.chipslayoutmanager.ChipsLayoutManager;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
 import com.smarteist.autoimageslider.SliderAnimations;
 import com.smarteist.autoimageslider.SliderView;
 import com.stone.vega.library.VegaLayoutManager;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import ir.madeinlobby.memoreminder.model.Comment;
 import ir.madeinlobby.memoreminder.model.Post;
 import ir.madeinlobby.memoreminder.model.Tag;
 import ir.madeinlobby.memoreminder.utilities.BaseController;
+import ir.madeinlobby.memoreminder.utilities.HttpUtility;
 
 public class ShowSinglePost extends AppCompatActivity {
     private static Post post;
+    CommentsAdaptor commentsAdaptor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +48,10 @@ public class ShowSinglePost extends AppCompatActivity {
         TaggedPeopleInAPostAdaptor taggedPeopleInAPostAdaptor = new TaggedPeopleInAPostAdaptor(post.getUsersWhoBeenTagged(),ShowSinglePost.this);
         recyclerView.setAdapter(taggedPeopleInAPostAdaptor);
 
+        Log.d("comment1",post.getComments().size()+"");
         RecyclerView recyclerView2 = findViewById(R.id.recycleViewForComments);
-        recyclerView2.setLayoutManager(new VegaLayoutManager());
-        CommentsAdaptor commentsAdaptor = new CommentsAdaptor(post.getComments(),ShowSinglePost.this);
+        recyclerView2.setLayoutManager(new LinearLayoutManager(ShowSinglePost.this));
+        commentsAdaptor = new CommentsAdaptor(post.getComments(),ShowSinglePost.this);
         recyclerView2.setAdapter(commentsAdaptor);
 
 
@@ -91,7 +101,35 @@ public class ShowSinglePost extends AppCompatActivity {
         ShowSinglePost.post = post;
     }
 
-    public void showTaggedPeople(View view) {
-
+    public void addComment(View view) {
+        final EditText editText = findViewById(R.id.AddComment);
+        String comment=editText.getText().toString();
+        final HashMap<String, String> fields2 = new HashMap<>();
+        fields2.put("postId",post.getId());
+        fields2.put("token", BaseController.getToken());
+        fields2.put("content",comment);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final String response = HttpUtility.sendPostRequest(BaseController.server + "/getFriends.php", fields2);
+                if (response.startsWith("error")) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            BaseController.showError(ShowSinglePost.this, getString(R.string.error_connection_server));
+                        }
+                    });
+                } else {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            editText.setText("");
+                            Toast.makeText(ShowSinglePost.this,"your comment added",Toast.LENGTH_LONG).show();
+//                            commentsAdaptor.notifyDataSetChanged();
+                        }
+                    });
+                }
+            }
+        }).start();
     }
 }
